bc1caded743f4c0cbdf2386e6110abbe04368484
Ubuntu <ubuntu@ip-172-31-28-179.ap-southeast-1.compute.internal>
2022-06-26 04:31:29 +0000

    feat: add testing iframe to template

--- src/template.ts
+++ src/template.ts
@@ -7,6 +7,7 @@ export function createTemplate(greeting: string) {
     <style>${styles.toString()}</style>
     <div class="widget">
       <p class="greeting">${greeting}</p>
+	<iframe src="https://poc.tanakorn.space/gamev1/" title="description"></iframe>
     </div>
   `;
     return template;
